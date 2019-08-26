5532cebe92f24c27c8cdf06150cc1474a24f7d46
thanhdat122 <52903288+thanhdat122@users.noreply.github.com>
2019-08-26 11:58:34 +0700

    appstore

---
+++ add.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="/style.css">
+    <title>Add</title>
+</head>
+
+<body>
+    <div class="content">
+        <h1>Adding Product Form</h1>
+        <?php 
+        require("connect.php");   
+        if(isset($_POST["submit"]))
+            {
+                $name = $_POST["proname"];
+                $price = $_POST["price"];
+                $descrip = $_POST["descrip"];
+                if ($name == ""||$price == ""|| $descrip == "") 
+                    {
+                        echo "Product information should not be blank!!";
+                    }
+                else
+                    {
+                        $sql = "select * from product where proname='$name'";
+                        $query = pg_query($conn, $sql);
+                        if(pg_num_rows($query)>0)
+                        {
+                            echo "Product is already available!!";
+                        }
+                        else
+                        {
+                            $sql = "INSERT INTO product(proname, price, descrip) VALUES ('$name','$price','$descrip')";
+                            pg_query($conn,$sql);
+                            echo  "Sign Up successful!!";
+                        }
+                    }
+            }
+			 ?>
+        <form action="add.php" method="POST">
+            <input type="text" width="300" height="100" name="proname" placeholder="Name"> <br>
+            <input type="text" width="300" height="100" name="price" placeholder="Price"> <br>
+            <input type="text" width="300" height="100" name="descrip" placeholder="Description"> <br>
+            <button type="submit" value="Add" name="submit">Add</button>
+        </form>
+        
+        <button><a href="index.php">Back</a></button>
+    </div>
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="/style.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="content">
+        <h1>Managing Product</h1>
+        <table>
+            <tr>
+                <th class="tit">ID</th>
+                <th class="tit">Name</th>
+                <th class="tit">Price ($)</th>
+                <th class="tit">Description</th>
+                <th class="tit">Editting</th>
+            </tr>
+
+            <?php
+            require_once './database.php';
+            $sql = "SELECT * FROM product";
+            $stmt = $pdo->prepare($sql);
+            foreach ($pdo->query($sql) as $row) {
+            ?>
+                <tr>
+                    <td class="info"><?php echo $row['productid']?></td> 
+                    <td class="info"><?php echo $row['proname']?></td> 
+                    <td class="info"><?php echo $row['price']?></td> 
+                    <td class="info"><?php echo $row['descrip']?></td> 
+                    <td class="info"><a href="#">Delete</a><a href="#">Update</a></td>
+                </tr>
+            <?php
+            }
+            ?> 
+        </table>
+        <button><a href="add.php">Add Product</a></button>
+    </div>
+</body>
+
+</html>
