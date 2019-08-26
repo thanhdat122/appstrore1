72438ed9c4c77550d1f47c6e1e7e22044a86eacd
thanhdat122 <52903288+thanhdat122@users.noreply.github.com>
2019-08-26 17:31:30 +0700

    thay doi

--- connect.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-$conn = pg_connect("host=ec2-107-20-155-148.compute-1.amazonaws.com port=5432 dbname=d5ufu4523q420h user=jwkrnprtpqjarr password=2179c242c2f73b05ce346e19d286eef8718b40f5827deedeaf5575c4ccecac6d") or die("Connect Unsuccessful");
-?>
